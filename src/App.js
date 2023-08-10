979d2be53e421a4e5130c3f51d375755dd63f407
Tikos174 <deryadinalex@bk.ru>
2023-08-10 12:11:37 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Main from "./components/Main/Main";
 import Header from "./components/Header/header";
 import SearchForm from './components/Movies/SearchForm/SearchForm'
 import Footer from "./components/Footer/Footer";
+import Register from "./components/Register/Register"
 
 function App() {
   return (
     <div className="app">
       <Header />
       {/* <Main /> */}
-      <SearchForm/>
+      {/* <SearchForm/> */}
+      {<Register />}
       <Footer/>
     </div>
 
---
+++ src/components/Register/Register.js
@@ -0,0 +1,29 @@
+function Register() {
+    return (
+        <section className="register">
+            <h2 className="register__title">Привет</h2>
+            <div className="register__form">
+                <p className="">
+                    !!!!!!
+                </p>
+                <p className="">
+                    !!!!!!
+                </p>
+                <p className="">
+                    !!!!!!
+                </p>
+                <p className="">
+                    !!!!!!
+                </p>
+            </div>
+            <a href="" className="">
+                Редактировать
+            </a>
+            <a href="" className="">
+                Выйти из аккаунта
+            </a>
+        </section>
+    )
+}
+
+export default Register
